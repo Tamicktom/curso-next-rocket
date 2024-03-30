ade00e5777b5ac6366a6509b52153a859470d72d
Tamicktom <rickpokemon38@gmail.com>
2024-03-30 18:23:26 -0300

    feat - add loading to search

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,7 +1,30 @@
+"use client";
+//* Libraries imports
+import { useSearchParams } from "next/navigation";
+
+//* Local imports
+import { Skeleton } from "@/components/skeleton";
+
 export default function LoadingSearch() {
+  const searchParams = useSearchParams();
+
   return (
-    <div>
-      <h1>Loading Search...</h1>
+    <div className="flex-col gap-4 flx">
+      <p className="text-sm">
+        Resultados para:{" "}
+        <span className="font-semibold">
+          {searchParams.get("q") ?? "Carregando..."}
+        </span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+      </div>
     </div>
   );
 }
