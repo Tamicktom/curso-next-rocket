4cb6a1fffc29296667ac8ac5d35511c7a4cedc2f
Tamicktom <rickpokemon38@gmail.com>
2024-03-30 18:52:33 -0300

    feat - add test for search

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,22 @@
+describe("search products", () => {
+  it("Should be able to search for a product and add it to the cart", () => {
+    cy.visit("/");
+
+    cy.get("input[name=q]").type("moletom").parent("form").submit();
+
+    cy.location("pathname").should("include", "/search");
+    cy.location("search").should("include", "q=moletom");
+
+    cy.get('a[href^="/product"]').should("exist");
+  });
+
+  it("Should not be able to visite search page without a search quey", () => {
+    cy.on("uncaught:exception", (err) => {
+      return false;
+    });
+
+    cy.visit("/search");
+
+    cy.location("pathname").should("equal", "/");
+  });
+});
